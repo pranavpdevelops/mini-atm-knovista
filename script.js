5b9779d4445daede6e067f24964ac005ebe70203
Pranav <pranavpatl2003@gmail.com>
2026-06-28 13:44:36 +0530

    new added

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
 let currentBalance =500
 
+let deposit = document.getElementById("deposit")
+let withdraww =document.getElementById("withdraw")
+let balance = document.getElementById("balance")
+
 
 
 while(true){
-let user = Number(prompt("  balance enter 1  deposit enter 2  withdraw enter 3  exit enter 4"))
+let user = Number(prompt("  balance enter 1,deposit enter 2 , withdraw enter 3,  exit enter 4"))
+
 if(user==1){
 
     alert(currentBalance)
 
 }else if(user==2){
-    let amt = Number(prompt("deposit ammout:"))
+    let amt = Number(prompt("deposit amout:"))
     currentBalance+=amt
-    console.log(`enter your amout$${amt}`)
+    deposit.textContent = `Deposited Amount ${amt}`
 }else if(user==3){
 
     let withdraw = Number(prompt("enter your withdrawal ammout:"))
 
     if(currentBalance>=withdraw){
            currentBalance-=withdraw
-           
-       console.log(`enter your withdrawal amout$${withdraw}`)
+           withdraww.textContent =`Withdrawal Amount ${withdraw} `
 
     }else{
         alert('insufficent balance')
     }
    
-    console.log("balance",currentBalance)
-
-    console.log("thank you visit again")
-    
+    balance.textContent=`Current Balance${currentBalance}`
+  
    
 
 }else if(user==4){
